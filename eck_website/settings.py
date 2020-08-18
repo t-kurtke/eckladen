8dea90162b273f0e598173abfb00dd9822211462
info2mt <thomas.keller7ca@gmail.com>
2020-08-18 19:12:41 +0200

    settings.py ready for deployment

--- eck_website/settings.py
+++ eck_website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n$zdn89jkdk*2_0qy1@@%b*qhnp9lgvjl&b7e&-e^uz+vbd+@='
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
