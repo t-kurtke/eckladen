a7b88b376038c24a982fccaa326b560ed1eb57b4
info2mt <thomas.keller7ca@gmail.com>
2020-08-20 00:24:41 +0200

    template dir

--- eck_website/settings.py
+++ eck_website/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'eck_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+         'DIRS': [os.path.join(BASE_DIR, 'eck_webiste/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
